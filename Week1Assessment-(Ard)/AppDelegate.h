68d3cf68ff2819fe99e488813a5b383f9c86553c
ardramli <adliramli88@gmail.com>
2017-03-20 13:55:11 +0800

    Initial Commit

---
+++ Week1Assessment-(Ard)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Week1Assessment-(Ard)
+//
+//  Created by ardMac on 20/03/2017.
+//  Copyright © 2017 ardMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
